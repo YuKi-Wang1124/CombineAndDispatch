e40cfca94cf2d1b4ffee24a2c1f2d1f779cafa32
mandy <mandy@newpos.tw>
2025-04-25 17:48:51 +0800

    Initial Commit

---
+++ CombineAndDispatch/CombineAndDispatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAndDispatchApp.swift
+//  CombineAndDispatch
+//
+//  Created by 王昱淇 on 2025/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineAndDispatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineAndDispatchTests/CombineAndDispatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAndDispatchTests.swift
+//  CombineAndDispatchTests
+//
+//  Created by 王昱淇 on 2025/4/25.
+//
+
+import Testing
+@testable import CombineAndDispatch
+
+struct CombineAndDispatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
